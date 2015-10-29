059ad52fd5c29e61fd92c3e7291091f2290a5260
ssturniolo <simone.sturniolo@gmail.com>
2015-10-29 17:40:38 +0000

    Added XRD method

--- soprano/calculate/xrd/xrd.py
+++ soprano/calculate/xrd/xrd.py
@@ -12,12 +12,12 @@ from __future__ import unicode_literals
 import numpy as np
 from collections import namedtuple
 # Internal imports
-from soprano.utils import hkl2d2_matgen
+from soprano.utils import hkl2d2_matgen, minimum_supcell, inv_plane_dist
 
 XraySpectrum = namedtuple("XraySpectrum", ("theta2", "hkl", "I", "lambdax"))
 
 
-def xrd_pwd_peaks(latt_abc, lambdax=1.54056, theta2_tol=1e-6):
+def xrd_pwd_peaks(latt_abc, lambdax=1.54056, theta2_digits=6):
     """
     Calculate the peaks (without intensities) of a powder
     XRD spectrum given the lattice in ABC form and the
@@ -28,10 +28,10 @@ def xrd_pwd_peaks(latt_abc, lambdax=1.54056, theta2_tol=1e-6):
     |                           Angstroms and radians
     |    lambdax (Optional[float]): X-ray wavelength in Angstroms
     |                               (default is 1.54056 Ang)
-    |    theta2_tol (Optional[float]): Tolerance within which
+    |    theta2_digits (Optional[int]): Rounding within which
     |                                  two theta angles (in degrees)
     |                                  are considered to be equivalent
-    |                                  (default is 1e-6 deg)
+    |                                  (default is 6 digits)
 
     | Returns:
     |    xpeaks (XraySpectrum): a named tuple containing the peaks
@@ -44,9 +44,51 @@ def xrd_pwd_peaks(latt_abc, lambdax=1.54056, theta2_tol=1e-6):
     """
 
     # First a sanity check
-    latt_abc = np.array(latt_abc)
+    latt_abc = np.array(latt_abc, copy=False)
     if latt_abc.shape != (2, 3):
         raise ValueError("Invalid argument latt_abc passed to xrd_pwd_peaks")
 
+    inv_d_max = 2.0/lambdax  # Upper limit to the inverse distance
+
     # Second, find the matrix linking hkl indices to the inverse distance
     hkl2d2 = hkl2d2_matgen(latt_abc)
+    hkl_bounds = minimum_supcell(inv_d_max, r_matrix=hkl2d2)
+
+    hrange = range(-hkl_bounds[0], hkl_bounds[0]+1)
+    krange = range(-hkl_bounds[1], hkl_bounds[1]+1)
+    lrange = range(-hkl_bounds[2], hkl_bounds[2]+1)
+
+    # Now generate a list of peaks based on their hkl values
+
+    xpeaks = XraySpectrum([], [], [], lambdax)
+
+    for h in hrange:
+        for k in krange:
+            for l in lrange:
+
+                hkl = [h, k, l]
+                inv_d = inv_plane_dist(hkl, hkl2d2)
+
+                # Some will still have inv_d > inv_d_max, we fix that here
+                # We also eliminate both 2theta = 0 and 2theta = pi to avoid
+                # divergence later in the geometric factor
+                if inv_d < inv_d_max and inv_d > 0:
+                    theta = np.arcsin(inv_d/inv_d_max)
+                    # Theta needs to be truncated to a certain number of
+                    # digits to avoid the same peak to be wrongly
+                    # considered as two
+                    theta2 = round(2.0*theta*180/np.pi, theta2_digits)
+                    if theta2 not in xpeaks.theta2:
+                        xpeaks.theta2.append(theta2)
+                        xpeaks.hkl.append([hkl])
+                        xpeaks.I.append(0.0)
+                    else:
+                        theta2_i = xpeaks.theta2.index(theta2)
+                        xpeaks.hkl[theta2_i] += [hkl]
+
+    # Now on to sorting by theta2
+    sorted_th2, sorted_hkl = zip(*sorted(zip(xpeaks.theta2, xpeaks.hkl),
+                                         key=lambda x: x[0]))
+    xpeaks = XraySpectrum(sorted_th2, sorted_hkl, xpeaks.I, lambdax)
+
+    return xpeaks
--- soprano/utils.py
+++ soprano/utils.py
@@ -108,53 +108,122 @@ def inv_plane_dist(hkl, hkl2d2):
     return np.sqrt(np.dot(np.dot(hkl2d2, hkl), hkl)[0, 0])
 
 
-def minimum_supcell(latt_cart, max_r):
-
-    """ Generate the bounds for a supercell containing a sphere
-        of given radius, knowing the unit cell.
+def minimum_supcell(max_r, latt_cart=None, r_matrix=None):
+    """
+    Generate the bounds for a supercell containing a sphere
+    of given radius, knowing the unit cell.
 
     | Args:
-    |   latt_cart (np.ndarray): unit cell in cartesian form
     |   max_r (float): radius of the sphere contained in the supercell
-    
+    |   latt_cart (np.ndarray): unit cell in cartesian form
+    |   r_matrix (np.ndarray): matrix for the quadratic form returning
+    |                          r^2 for this supercell.
+    |                          Alternative to latt_cart, for a direct
+    |                          space cell would be equal to
+    |                          np.dot(latt_cart, latt_cart.T)
+
     | Returns:
     |   bounds (tuple[int]): bounds of the supercell to be built.
-    |                        These are to be interpreted as the 
-    |                        thickness of the "shell" one has to build 
+    |                        These are to be interpreted as the
+    |                        thickness of the "shell" one has to build
     |                        around a core unit cell: for example (1,2,1)
     |                        means the supercell will be 3x5x3
-    
+
     | Raises:
     |   ValueError: if some of the arguments are invalid
 
     """
 
-    latt_cart = np.array(latt_cart)
-    if (latt_cart.shape != (3,3)):
-        raise ValueError("Invalid latt_cart passed to minimum_supcell")
+    if latt_cart is not None:
+        latt_cart = np.array(latt_cart, copy=False)
+        if latt_cart.shape != (3, 3):
+            raise ValueError("Invalid latt_cart passed to minimum_supcell")
+        r_matrix = np.dot(latt_cart, latt_cart.T)
+    elif r_matrix is not None:
+        r_matrix = np.array(r_matrix, copy=False)
+        if r_matrix.shape != (3, 3):
+            raise ValueError("Invalid r_matrix passed to minimum_supcell")
+    else:
+        raise ValueError("One between latt_cart and r_matrix has to \
+                          be present")
 
     # The logic of this algorithm isn't obvious, so let's make it clearer.
-    # 
-    # By diagonalizing it we can calculate the transformation matrix which morphs a simple unit sphere into this ellipsoid
-    # through scaling (with the eigenvalues) and rotating (with the eigenvectors).
-    # We have thus that a point p in unit sphere space becomes q = M*p in our space of interest (that is, hkl space). On the other hand,
-    # a point q in hkl space will be transformed into unit sphere space by p = M^-1*q. 
-    # Now, the boundaries of the ellipsoids are none other but the points which, in hkl space, have normals aligned with the axes.
-    # And NORMALS transform between spaces with a matrix that is the inverse transpose of the regular one.
-    # So the logic goes:
-    # - take an axis direction in hkl space (for example, [1,0,0])
-    # - transform it into unit sphere space by premultiplying (M^-1)^-1^T = M^T
-    # - now that we have the direction of the normal in unit sphere space, we just need to normalize it to find the point p which has that normal
-    #   (thanks to the properties of the unit sphere)
-    # - then we transform back to hkl space with M and there you go, that's your boundary point.
-    # - wash, rinse, repeat for all axes directions. Find the maxima in each direction and you've got your box.
+    #
+    # What we are basically looking for is the AABB bounding box of a sphere
+    # in absolute space.
+    # This becomes a rotated ellipsoid in fractional coordinates space.
+    # r_matrix represents a quadratic form which turns a fractional coordinate
+    # into a squared distance in space.
+    # By diagonalizing it we can calculate the transformation matrix
+    # which morphs a simple unit sphere into this ellipsoid through
+    # scaling (with the eigenvalues) and rotating (with the eigenvectors).
+    # We have thus that a point p in unit sphere space becomes q = M*p
+    # in fractional space. On the other hand, a point q in fractional space
+    # will be transformed into absolute space by p = M^-1*q.
+    # Now, the boundaries of the ellipsoids are none other but the points
+    # which, in regular space, have normals aligned with the axes.
+    # And NORMALS transform between spaces with a matrix that is the
+    # inverse transpose of the regular one. So the logic goes:
+    # - take an axis direction in fractional space (for example, [1,0,0])
+    # - transform it into absolute space by premultiplying (M^-1)^-1^T = M^T
+    # - now that we have the direction of the normal in absolute space,
+    # we just need to normalize it to find the point p which has that normal
+    # (thanks to the properties of the unit sphere)
+    # - then we transform back to fractional space with M and there you go,
+    # that's your boundary point.
+    # - wash, rinse, repeat for all axes directions.
+    # Find the maxima in each direction and you've got your box.
 
-    r_Matrix = np.dot(latt_cart, latt_cart.T)
-    r_evals, r_evecs = np.linalg.eigh(r_Matrix)
+    r_evals, r_evecs = np.linalg.eigh(r_matrix)
 
-    unit_transf_M = np.dot(r_evecs, np.diag(1.0/np.sqrt(r_evals)))          # Unit sphere - to - ellipsoid transformation matrix
-    # To find the boundaries, we need to iterate over the three main directions. We do this implicitly though
-    qMatrix = np.dot(unit_transf_M, max_r*(unit_transf_M/np.linalg.norm(unit_transf_M, axis=1)[:,None]).T)
-    r_bounds = np.max(np.ceil(abs(qMatrix)), axis=1).astype(int)
+    # Unit sphere - to - ellipsoid transformation matrix
+    utransf_matrix = np.dot(r_evecs, np.diag(1.0/np.sqrt(r_evals)))
+    # To find the boundaries, we need to iterate over the three main
+    # directions. We do this implicitly though.
+    qmatrix = np.dot(utransf_matrix,
+                     max_r*(utransf_matrix/np.linalg.norm(utransf_matrix,
+                                                         axis=1)[:, None]).T)
+    r_bounds = np.max(np.ceil(abs(qmatrix)), axis=1).astype(int)
 
     return tuple(r_bounds)
+
+
+def supcell_gridgen(latt_cart, r_bounds):
+    """
+    Generate a full linearized grid for a supercell with r_bounds
+    and a base unit cell in Cartesian form.
+
+    | Args:
+    |   latt_cart (np.ndarray): unit cell in cartesian form
+    |   r_bounds (tuple[int]): bounds of the supercell to be built,
+    |                          as returned by minimum_supcell.
+
+    | Returns:
+    |   neigh_i_grid (np.ndarray): supercell grid in fractional coordinates
+    |   neigh_grid (np.ndarray): supercell grid in cartesian coordinates
+
+    | Raises:
+    |   ValueError: if some of the arguments are invalid
+
+    """
+
+    latt_cart = np.array(latt_cart, copy=False)
+    if latt_cart.shape != (3, 3):
+        raise ValueError("Invalid latt_cart passed to minimum_supcell")
+
+    r_bounds = np.array(r_bounds, copy=False)
+    if r_bounds.shape != (3,):
+        raise ValueError("Invalid r_bounds passed to minimum_supcell")
+
+    x_range = range(-r_bounds[0], r_bounds[0]+1)
+    y_range = range(-r_bounds[1], r_bounds[1]+1)
+    z_range = range(-r_bounds[2], r_bounds[2]+1)
+
+    # We now generate a grid of neighbours to check for contact with
+    # First just the supercell indicess
+    neigh_i_grid = np.swapaxes(np.meshgrid(x_range, y_range, z_range), 0, 3)
+    neigh_i_grid = np.reshape(neigh_i_grid, (-1, 3))
+    # Then the actual supercell cartesian shifts
+    neigh_grid = np.dot(neigh_i_grid, latt_cart)
+
+    return neigh_i_grid, neigh_grid
