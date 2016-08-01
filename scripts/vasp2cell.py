6b8a2170794fd6593e999568aecb7d11058c0555
ssturniolo <simone.sturniolo@gmail.com>
2016-08-01 16:15:45 +0100

    vasp2cell.py completed

--- scripts/vasp2cell.py
+++ scripts/vasp2cell.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python
-
 """ A very simple script to convert from all ASE-accepted formats to CASTEP's
 .cell format"""
+
+# Python 2-to-3 compatibility code
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
 
 import os
 import glob
@@ -15,7 +20,32 @@ parser.add_argument('input_folders', type=str, nargs='+', default=None,
 
 args = parser.parse_args()
 
+_inpfiles = ('POSCAR', 'CONTCAR')
+
 for inpf in args.input_folders:
     # Check that these folders have the right files
-    foldf = glob.glob(os.path.join(inpf, '*'))
-    
+    a = None
+    for ft in _inpfiles:
+        try:
+            a = io.read(os.path.join(inpf, ft))
+        except IOError as e:
+            if "Could not determine chemical symbols" in str(e):
+                # Wrong header format!
+                print("WARNING - ASE could not read elements in POSCAR file. "
+                      "\nBy convention, elements should be elencated in the "
+                      "first line of the POSCAR file in the order in which "
+                      "they appear in the line in which the number of each "
+                      "atom type is given.")
+            continue
+    if a is None:
+        print("No valid structure files found in folder {0}\n".format(inpf) +
+              "Skipping...")
+        continue
+
+    name = os.path.split(inpf)[-1]
+    io.write(name + '.cell', a)
+
+
+
+
+
