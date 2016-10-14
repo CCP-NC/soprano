732d358a27e5ae68cbd3dd11f27facfaa34bc27d
ssturniolo <simone.sturniolo@gmail.com>
2016-10-14 13:59:31 +0100

    Added safety mechanisms for 'soft' crash of Submitters

--- soprano/hpc/submitter/_spawn.py
+++ soprano/hpc/submitter/_spawn.py
@@ -9,8 +9,22 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 import sys
+import shutil
 
 # Spawn a Submitter instance from a given module
 subm_module, subm_name = sys.argv[1:3]
 loaded_module = __import__(subm_module, fromlist=[subm_name])
-getattr(loaded_module, subm_name).start()
+subm = getattr(loaded_module, subm_name)
+try:
+    subm.start()
+except Exception as e:
+    # If ANYTHING goes wrong, at least clean up!
+    for j in subm._jobs:
+        subm.queue.kill(j)
+        try:
+            shutil.rmtree(subm._jobs[j]['folder'])
+        except OSError:
+            pass # Whatever, it was deleted already I guess
+    subm.log('Submitter crashed following error:'
+             '\n{0}:\n\t{1}'.format(type(e).__name__, e))
+
