df3bb5874b729b37e7074d1c5f65985b49c077e7
ssturniolo <simone.sturniolo@gmail.com>
2016-10-21 16:56:12 +0100

    hpc.multiprocessing removed

--- soprano/hpc/multiprocessing/__init__.py
+++
@@ -1,8 +0,0 @@
-"""HPC Daemons are meant to run one for each node of a cluster machine.
-They actually manage the running of multi-core processes at a local level.
-"""
-
-# Python 2-to-3 compatibility code
-from __future__ import absolute_import
-
-from soprano.hpc.multiprocessing.daemon import (DaemonHPC, DaemonRunner,)
