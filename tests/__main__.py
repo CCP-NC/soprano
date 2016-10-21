f3252b9ae4b30c05532fda772258c39efe1018ff
ssturniolo <simone.sturniolo@gmail.com>
2016-10-21 17:59:13 +0100

    Added timing to tests/__main__

--- tests/__main__.py
+++ tests/__main__.py
@@ -1,15 +1,19 @@
 """Run all tests in this folder"""
 
+import time
 import os, sys
 import glob
 import subprocess as sp
 
 tests_py = glob.glob(os.path.join(os.path.dirname(__file__), '*tests.py'))
 failed = []
+t0 = time.time()
 for t in tests_py:
     if sp.Popen(['python', t]).wait() != 0:
         failed += [t]
 
+print("All tests completed")
+print("Time spent: {0: .3f} s".format(time.time()-t0))
 if len(failed) > 0:
     sys.stdout.write("The following tests failed:\n\t")
     print("\n\t".join(failed))
