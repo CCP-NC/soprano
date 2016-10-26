c57a9f2bf83f06e344d51667585fb8cb518209e9
ssturniolo <simone.sturniolo@gmail.com>
2016-10-26 13:30:21 +0100

    Added LGPL headers to source files

--- soprano/hpc/__init__.py
+++ soprano/hpc/__init__.py
@@ -1,3 +1,19 @@
+# Soprano - a library to crack crystals! by Simone Sturniolo
+# Copyright (C) 2016 - Science and Technology Facility Council
+
+# Soprano is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# Soprano is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 """Classes and functions useful to run calculations on huge cluster systems
 (High Performance Computation). To be used with care - these are liable to
 fail if some specific architecture has quirks that are not accounted for!
