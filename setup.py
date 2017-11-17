49e392ee68674a769a9250ec2b305fca86d89b9e
Simone Sturniolo <simonesturniolo@gmail.com>
2017-11-17 16:21:19 +0000

    Prepared setup.py for uploading on PyPi

--- setup.py
+++ setup.py
@@ -15,9 +15,50 @@ from __future__ import print_function
 from setuptools import setup, find_packages
 from soprano import __version__
 
+long_description = """
+Soprano is a Python library developed and maintained by the CCP for NMR
+Crystallography as a tool to help scientists working with crystallography and
+simulations to generate, manipulate, run calculations on and analyse large
+data sets of crystal structures, with a particular attention to the output of
+ab-initio random structure searching,
+or AIRSS. (https://www.mtg.msm.cam.ac.uk/Codes/AIRSS)
+
+It provides a number of functionalities to help automate many common tasks
+in computational crystallography."""
+
 if __name__ == '__main__':
     setup(name='Soprano',
           version=__version__,
+          description='A Python library to crack crystals',
+          long_description=long_description,
+          url='https://ccp-nc.github.io/soprano/',
+          author='Simone Sturniolo',
+          author_email='simone.sturniolo@stfc.ac.uk',
+          license='LGPL',
+          classifiers=[
+              # How mature is this project? Common values are
+              #   3 - Alpha
+              #   4 - Beta
+              #   5 - Production/Stable
+              'Development Status :: 4 - Beta',
+
+              # Indicate who your project is intended for
+              'Intended Audience :: Science/Research',
+              'Topic :: Scientific/Engineering :: Chemistry',
+              'Topic :: Scientific/Engineering :: Physics',
+              'Topic :: Scientific/Engineering :: Information Analysis',
+
+              # Pick your license as you wish (should match "license" above)
+              'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+
+              # Specify the Python versions you support here. In particular, ensure
+              # that you indicate whether you support Python 2, Python 3 or
+              # both.
+              'Programming Language :: Python :: 2',
+              'Programming Language :: Python :: 2.7',
+              'Programming Language :: Python :: 3',
+          ],
+          keywords='crystallography ccpnc computational chemistry',
           packages=find_packages(),
           # For data files. Example: 'soprano': ['data/*.json']
           package_data={'soprano': ['data/*.json']},
@@ -29,5 +70,6 @@ if __name__ == '__main__':
               'numpy',
               'scipy',
               'ase'
-          ]
+          ],
+          python_requires='>=2.7'
           )
