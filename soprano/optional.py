ac13e093cd55909195f9607925985e1a0cb3e5db
Simone Sturniolo <simonesturniolo@gmail.com>
2018-03-27 15:29:23 +0100

    Added module for optional imports

---
+++ soprano/optional.py
@@ -0,0 +1,61 @@
+# Soprano - a library to crack crystals! by Simone Sturniolo
+# Copyright (C) 2016 - Science and Technology Facility Council
+
+# Soprano is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# Soprano is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+optional.py
+
+Contains imports for libraries considered optional, and decorators to include
+checks for them.
+"""
+
+# Python 2-to-3 compatibility code
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+try:
+    import networkx as _networkx
+except ImportError:
+    _networkx = None
+
+
+"""
+These decorators check if the required module is available, if not print
+an error message, if yes pass it as a variable to the function itself. 
+They all take the name one desires the library to have within the function
+as an argument. The function needs to have a named variable of the same name
+in its interface.
+"""
+
+
+def requireNetworkX(import_name='networkx'):
+
+    def decorator(func):
+
+        def wrapper(*args, **kwargs):
+
+            if _networkx is None:
+                print('This function requires an installation of NetworkX'
+                      ' to work - please install it with\n\tpip install'
+                      ' networkx')
+            else:
+                kwargs[import_name] = _networkx
+                return func(*args, **kwargs)
+
+        return wrapper
+
+    return decorator
