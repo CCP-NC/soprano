6aa0af0418be1f2b7b1a0127a75d378827ce7f7f
Simone Sturniolo <simone.sturniolo@gmail.com>
2018-12-05 12:36:47 +0000

    Switched to an entry point format for scripts

---
+++ soprano/scripts/__init__.py
@@ -0,0 +1,22 @@
+# Soprano - a library to crack crystals! by Simone Sturniolo
+# Copyright (C) 2016 - Science and Technology Facility Council
+
+# Soprano is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# Soprano is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Python 2-to-3 compatibility code
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
