488eaf122c4d18d9e4b2506e83b40e52c63eb2f5
Simone Sturniolo <simonesturniolo@gmail.com>
2021-01-12 11:26:35 +0000

    Updated version number to 0.8.9

--- soprano/__init__.py
+++ soprano/__init__.py
@@ -39,5 +39,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 from __future__ import absolute_import
 
 # Version definition
-__version__ = '0.8.8'
+__version__ = '0.8.9'
 __release__ = 'beta (Boggess)'
