49b5e07b083618e5145f2d10e2a94b7e07ceba23
Kane Shenton <kane.shenton@stfc.ac.uk>
2023-03-02 12:19:27 +0000

    tests for merge sites utility

--- tests/utils_tests.py
+++ tests/utils_tests.py
@@ -160,6 +160,68 @@ class TestSupercellMethods(unittest.TestCase):
         vp, vcells = minimum_periodic(v, c)
         self.assertTrue(np.isclose(np.linalg.norm(vp, axis=-1), 0.1).all())
 
+# test the merging of sites
+class TestMergeSites(unittest.TestCase):
+
+
+    def test_merge_sites(self):
+        from soprano.utils import merge_sites
+        from ase.build import molecule
+        # define a structure (let's use ethanol)
+        atoms = molecule('CH3CH2OH', vacuum=10, pbc=True) # ethanol
+        # set some custom arrays
+        ms = np.random.rand(len(atoms), 3, 3)
+        efg = np.random.rand(len(atoms), 3)
+        labels = np.array(['C1', 'C2', 'O1', 'H1', 'H2', 'H3', 'H4', 'H5', 'H6'])
+        atoms.set_array('ms', ms)
+        atoms.set_array('efg', efg)
+        atoms.set_array('labels', labels)
+        
+        # define the indices of the sites to merge
+        # use the CH3 group as an example
+        merge_indices = [6, 7, 8] # the H atoms from the CH3 group
+        # merge the sites
+        atoms = merge_sites(atoms, merge_indices)
+        # check that the number of atoms is correct
+        self.assertEqual(len(atoms), 7)
+        # check that the CH3 group is now a single site
+        self.assertEqual(atoms.get_array('labels')[6], 'H4--H5--H6')
+        # check that the ms and efg arrays are correct
+        self.assertTrue(np.allclose(atoms.get_array('ms')[6], np.mean(ms[merge_indices], axis=0)))
+        self.assertTrue(np.allclose(atoms.get_array('efg')[6], np.mean(efg[merge_indices], axis=0)))
+
+        # now let's try to merge the C1 and C2 sites
+        merge_indices = [0, 1]
+        atoms = merge_sites(atoms, merge_indices)
+        # check that the number of atoms is correct
+        self.assertEqual(len(atoms), 6)
+        # check that the C1 and C2 sites are now a single site
+        self.assertEqual(atoms.get_array('labels')[0], 'C1--C2')
+        # check that the ms and efg arrays are correct
+        self.assertTrue(np.allclose(atoms.get_array('ms')[0], np.mean(ms[merge_indices], axis=0)))
+        self.assertTrue(np.allclose(atoms.get_array('efg')[0], np.mean(efg[merge_indices], axis=0)))
+
+
+        # we can also change the behaviour of the merge_sites function
+        # by passing a custom function to the merge_func argument
+        # let's merge the CH2 groups by taking first of each quantity
+        atoms = merge_sites(atoms, [2, 3], merging_strategies={'ms': lambda x: x[0],
+                                                               'efg': lambda x: x[0],
+                                                               'labels': lambda x: x[0]})
+        # check that the number of atoms is correct
+        self.assertEqual(len(atoms), 5)
+        # check that the CH2 groups are now a single site
+        # here the group got the label of the first atom in the group
+        self.assertEqual(atoms.get_array('labels')[2], 'H1')
+        # check that the ms and efg arrays are correct
+        # since C1 and C2 are merges in atoms, but not in ms and efg
+        # we need to shift the indices by 1
+        self.assertTrue(np.allclose(atoms.get_array('ms')[2], ms[3]))
+        self.assertTrue(np.allclose(atoms.get_array('efg')[2], efg[3]))
+
+
+
+        
 
 if __name__ == "__main__":
     unittest.main()
