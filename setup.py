2e30fe095a2bb84a506abb647c38fc189afe42cd
Kane Shenton <kane.shenton@stfc.ac.uk>
2024-07-04 12:55:44 +0100

    Update dependencies, docs config and automatic deployment of docs

--- setup.py
+++ setup.py
@@ -72,6 +72,6 @@ if __name__ == "__main__":
         use_2to3=False,
         convert_2to3_doctests=[],
         # Requirements
-        install_requires=["numpy>=1.18.5,<2.0", "scipy", "ase>=3.23"],
+        install_requires=["numpy>=1.18.5,<2.0", "scipy", "ase>=3.23", "spglib>=2.4"],
         python_requires=">=3.8",
     )
