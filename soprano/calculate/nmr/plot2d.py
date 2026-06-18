1a7b214c63a8528cc6b9d910e691fc1c5b0b629a
Kane Shenton <jkshenton@gmail.com>
2026-06-18 15:31:37 +0100

    fix add more missing typing imports

--- soprano/calculate/nmr/plot2d.py
+++ soprano/calculate/nmr/plot2d.py
@@ -1,7 +1,7 @@
 """2D NMR plotting orchestrator."""
 
 import logging
-from typing import Any, Optional
+from typing import Any, List, Optional, Tuple, Union
 
 import numpy as np
 from matplotlib.axes import Axes
